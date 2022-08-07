1784ced228a9e499809cff2a8aa2b550b2438dda
Gellert Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2022-08-07 18:41:37 +0100

    Hreflang tags

--- src/components/base-page.tsx
+++ src/components/base-page.tsx
@@ -4,19 +4,42 @@ import Head from "next/head"
 
 import Footer from "@components/footer"
 import Navbar from "@components/navbar"
+import { useRouter } from "next/router";
+import { data } from "@content/components/base-page";
 
 export default function BasePage(props) {
+  const { locale, locales, asPath, pathname, basePath, defaultLocale } = useRouter();
+  const content = data[locale];
+
+  const hostname = process.env['HOST'] || process.env['NEXT_PUBLIC_HOST'];
+  console.log("Host:", hostname);
+
   return (
     <div>
       <Head>
-        <title>{props.title} - Perident Dental Clinic Gyor</title>
+        <title>Perident Dental Clinic Gyor, Hungary</title>
 
         <meta
           name="description"
-          content="Painless aesthetic dentistry in Győr. Implants, 3D CT, panoramic X-ray, dental prosthesis, bridges, crowns, prosthesis, denture fixation and teeth whitening."
+          content={content.meta_description}
         />
-
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content={content.meta_description}
+        />
+        {locales.map((loc) => {
+                    const prefix = loc === defaultLocale ? "" : loc;
+                    const path = loc === defaultLocale ? asPath.substring(1) : asPath;
+                    return (
+                        <link
+                            key={loc}
+                            rel="alternate"
+                            hrefLang={loc}
+                            href={`https://${hostname}/${prefix}${path}`}
+                        />
+                    )
+                })}
+        <link key="x-default" rel="alternate" hrefLang="x-default" href={`https://${hostname}/${asPath.substring(1)}`} />
       </Head>
       <main className="mx-0 xl:mx-12">
         <Navbar/>
---
+++ src/content/components/base-page.tsx
@@ -0,0 +1,11 @@
+export const data = {
+    en: {
+      meta_description: "For a range of dental treatments visit our dental practice in Győr, Hungary. The Perident Dental Clinic boast a friendly team who offer a variety of services."
+    },
+    hu: {
+        meta_description: "Teljes körű fogászati kezeléseinkért látogasson el Győri fogászati rendelőnkbe. A Perident Fogászat barátságos csapata szeleskoru fogaszati szolgáltatásokat kínál."
+    },
+    de: {
+        meta_description: "Für eine Reihe von Zahnbehandlungen besuchen Sie unsere Zahnarztpraxis in Győr, Ungarn. Die Perident Dental Clinic verfügt über ein freundliches Team, das eine Vielzahl von Dienstleistungen anbietet."
+    },
+  }
