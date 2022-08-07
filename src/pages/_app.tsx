5c066eddc0fb99421705bd370f72896719acc0a6
Gellert Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2022-08-07 18:49:05 +0100

    Hotjar setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,13 @@ import React from "react"
 
 import { AppProps } from "next/app"
 import "../styles/tailwind.scss"
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    hotjar.initialize(Number(process.env["HJID"]), process.env["HJSV"])
+  }, [])
   return <Component {...pageProps} />
 }
 
