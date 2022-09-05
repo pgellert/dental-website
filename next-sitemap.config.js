c0217a43903332700065359f7cefec1444510ed2
Gellert Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2022-09-05 19:19:21 +0100

    fix: sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next-sitemap').IConfig} */
+const SITE_BASE = "https://" + process.env.HOST
 const config = {
-  siteUrl: "https://" + process.env.HOST,
+  siteUrl: SITE_BASE,
   generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
+  alternateRefs: [
+    {
+      href: SITE_BASE + "/en",
+      hreflang: 'en',
+    },
+    {
+      href: SITE_BASE + "/de",
+      hreflang: 'de',
+    },
+  ],
 }
 
 module.exports = config
