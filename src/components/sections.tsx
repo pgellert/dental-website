99892eb5b16dbae450dcfbd11f219f53156eb0af
Gellert Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2022-10-08 18:53:04 +0100

    page restructure

--- src/components/sections.tsx
+++ src/components/sections.tsx
@@ -1,9 +1,9 @@
 export function SectionTitle(props) {
   return (
     <div className="mt-4 flex w-full flex-col text-center">
-      <h3 className="mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 dark:text-white lg:text-4xl lg:leading-tight">
+      <h2 className="mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 dark:text-white lg:text-4xl lg:leading-tight">
         {props.title}
-      </h3>
+      </h2>
 
       <p className="py-4 px-12 text-lg leading-normal text-gray-500 dark:text-gray-300 lg:text-xl xl:text-xl">
         {props.subtitle}
--- src/content/components/hero.tsx
+++ src/content/components/hero.tsx
@@ -1,19 +1,19 @@
 export const data = {
   en: {
-    title: "Welcome to Perident Dental Clinic Gyor",
-    subtitle: "Dr. Peresztegi Szabolcs's dental practice",
+    title: "Dr. Peresztegi Szabolcs dentist - Győr, Hungary",
+    subtitle: "Welcome to the website of Perident Dentistry! We are at your service at our clinic in Győr. If you have any questions, feel free to call us.",
     booking: "Book an appointment",
     call: "Call us",
   },
   hu: {
-    title: "Üdvözöljük a Győri Perident Fogászatban",
-    subtitle: "Dr. Peresztegi Szabolcs fogorvosi rendelője",
+    title: "Dr. Peresztegi Szabolcs fogorvos - Győr",
+    subtitle: "Üdvözöljük a Perident Fogászat weboldalán! Győri rendelőnkbe várjuk kezelésre. Ha bármi kérdése van, hívjon minket bizalommal.",
     booking: "Időpont foglalás",
     call: "Hívjon minket",
   },
   de: {
-    title: "Willkommen in der Perident Zahnklinik Györ",
-    subtitle: "Die Zahnarztpraxis von Dr. Peresztegi Szabolcs",
+    title: "Dr. Peresztegi Szabolcs Zahnarzt - Győr, Ungarn",
+    subtitle: "Willkommen auf der Website von Perident Dentistry! Wir sind in unserer Klinik in Győr für Sie da. Wenn Sie Fragen haben, können Sie uns gerne anrufen.",
     booking: "Termin buchen",
     call: "Rufen Sie uns an",
   },
