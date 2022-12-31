b2ac651bf428217a7b3cc3399987cd1fa77474a3
Gellert Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2022-12-31 16:35:25 +0100

    fix: spacing around section title

--- src/components/sections.tsx
+++ src/components/sections.tsx
@@ -1,6 +1,6 @@
 export function SectionTitle(props) {
   return (
-    <div className="mt-4 flex w-full flex-col text-center">
+    <div className="mt-4 mx-2 flex w-full flex-col text-center">
       <h2 className="mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 dark:text-white lg:text-4xl lg:leading-tight">
         {props.title}
       </h2>
