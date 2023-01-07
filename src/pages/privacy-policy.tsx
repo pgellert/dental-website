0b1cd9a9c556c8875a64251c405f74267ac2c3ab
Gellert Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2023-01-07 15:20:48 +0100

    fix: privacy policy title

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,10 +1,26 @@
 import BasePage from "@components/base-page"
 import { Content } from "@components/service-page"
 
-export default function Home({ markdown }) {
+const data = {
+  en: {
+    title: "Privacy Policy | Perident Dentistry",
+    meta_description: "",
+  },
+  hu: {
+    title: "Adatvédelmi irányelvek | Perident Fogászat",
+    meta_description: "",
+  },
+  de: {
+    title: "Datenschutz-Bestimmungen | Zahnarztpraxis Perident",
+    meta_description: "",
+  },
+}
+
+export default function Home({ markdown, locale }) {
+  const content = data[locale];
   return (
     <div>
-      <BasePage title="" meta_description="" children="">
+      <BasePage title={content.title} meta_description={content.meta_description}>
         <Content markdown={markdown} />
       </BasePage>
     </div>
@@ -15,6 +31,7 @@ export async function getStaticProps({locale}) {
   return {
     props: {
       markdown: await require(`@markdowns/${locale == 'hu' ? locale : 'hu'}/privacy-policy.md`).default,
+      locale: locale,
     },
   }
 }
