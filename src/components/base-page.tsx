0d693a9b0b5b1d0cc049d5436e6d53d8074a69f9
Gellert Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2023-02-12 16:49:52 +0000

    Use Script tag for scripts

--- src/components/base-page.tsx
+++ src/components/base-page.tsx
@@ -64,7 +64,7 @@ export default function BasePage({title, meta_description, children}) {
 
         {/* Google tag manager */}
         {consentGiven ?
-          <script
+          <Script
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -76,7 +76,7 @@ export default function BasePage({title, meta_description, children}) {
           : <></>
         }
         
-        <script
+        <Script
           key="structured-data"
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structured_data) }}
