03196adbf778e20f7a136524c93770a97bee5349
Gellert Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2023-02-12 17:56:29 +0000

    Nextjs images in markdown

--- src/components/service-page.tsx
+++ src/components/service-page.tsx
@@ -5,6 +5,7 @@ import rehypeRaw from "rehype-raw"
 
 import Container from "@components/container"
 import Link from "next/link"
+import Image from "next/image"
 import BasePage from "./base-page"
 import { useRouter } from "next/router"
 import { data } from "@content/components/service-page"
@@ -40,6 +41,37 @@ export function Content(props) {
 
             </Link>)
           );
+        },
+        p: (paragraph: { children?: boolean; node?: any}) => {
+          const { node } = paragraph
+        
+          if (node.children[0].tagName === "img") {
+            const image = node.children[0]
+            const metastring = image.properties.alt
+            const alt = metastring?.replace(/ *\{[^)]*\} */g, "")
+            const metaWidth = metastring.match(/{([^}]+)x/)
+            const metaHeight = metastring.match(/x([^}]+)}/)
+            const width = metaWidth ? metaWidth[1] : "768"
+            const height = metaHeight ? metaHeight[1] : "432"
+            const isPriority = metastring?.toLowerCase().match('{priority}')
+            const hasCaption = metastring?.toLowerCase().includes('{caption:')
+            const caption = metastring?.match(/{caption: (.*?)}/)?.pop()
+        
+            return (
+              <div className="postImgWrapper">
+                <Image
+                  src={image.properties.src}
+                  width={width}
+                  height={height}
+                  className="postImg"
+                  alt={alt}
+                  priority={isPriority}
+                />
+                  {hasCaption ? <div className="caption" aria-label={caption}>{caption}</div> : null}
+              </div>
+            )
+          }
+          return <p>{paragraph.children}</p>
         },
       }}
       >
