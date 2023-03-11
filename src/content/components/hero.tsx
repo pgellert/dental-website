642fb5caa5eeea6d4ea04be58c327aab6ecf32b7
Gellert Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2023-03-11 21:24:34 +0000

    subtitle better

--- src/content/components/hero.tsx
+++ src/content/components/hero.tsx
@@ -1,19 +1,19 @@
 export const data = {
   en: {
     title: "Perident Dental Clinic Győr",
-    subtitle: "Since we have more than 20 years of experience, we can guarantee that we can help you achieve the beautiful smile you really want.",
+    subtitle: "The Perident team is dedicated to effectively assisting you with your dental problems so that you don't have to endure dental pain, discomfort, or imperfections.",
     booking: "Book an appointment",
     call: "Call us",
   },
   hu: {
     title: "Magán Fogorvos Győrben",
-    subtitle: "Mivel több mint 20 év tapasztalattal rendelkezünk, garantálni tudjuk, hogy hozzá tudjuk segíteni Önt ahhoz a gyönyörű mosolyhoz amire igazán vágyik.",
+    subtitle: "A Perident csapata arra törekszik, hogy hatékonyan segítsen Önnek fogászati problémáiban, hogy ne kelljen a fájdalmat, kényelmetlenséget vagy foghibákat sokáig elviselnie.",
     booking: "Időpontfoglalás",
     call: "Hívjon minket",
   },
   de: {
     title: "Perident Zahnklinik Győr",
-    subtitle: "Da wir über mehr als 20 Jahre Erfahrung verfügen, können wir Ihnen garantieren, dass wir Ihnen zu dem schönen Lächeln verhelfen können, das Sie sich wirklich wünschen.",
+    subtitle: "Das Perident-Team ist bestrebt, Sie bei Ihren Zahnproblemen effektiv zu unterstützen, damit Sie keine Zahnschmerzen, Beschwerden oder Unvollkommenheiten ertragen müssen.",
     booking: "Termin buchen",
     call: "Rufen Sie uns an",
   },
