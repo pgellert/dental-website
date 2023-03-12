0e75208c936e17f39fd75195598f15c9c43c48b7
Gellert Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2023-03-12 21:59:30 +0000

    next script

--- src/components/google-map.tsx
+++ src/components/google-map.tsx
@@ -1,20 +1,13 @@
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import { GoogleMap, MarkerF, LoadScriptNext } from "@react-google-maps/api";
 
 const center = { lat: 47.6663, lng: 17.6449 };
 
 export default function PeridentMap( {className} ) {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-  });
-
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Map className={className}/>;
-}
-
-function Map( {className} ) {
   return (
-    <GoogleMap zoom={16} center={center} mapContainerClassName={className}>
-      <MarkerF position={center}/>
-    </GoogleMap>
+    <LoadScriptNext googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY} >
+      <GoogleMap zoom={16} center={center} mapContainerClassName={className}>
+        <MarkerF position={center}/>
+      </GoogleMap>
+    </LoadScriptNext>
   );
 }
