95526853d47cb9b265a0b2ad12bffdfbda49a9f1
Gellert Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2023-03-12 22:01:43 +0000

    prevent google fonts

--- src/components/google-map.tsx
+++ src/components/google-map.tsx
@@ -4,7 +4,7 @@ const center = { lat: 47.6663, lng: 17.6449 };
 
 export default function PeridentMap( {className} ) {
   return (
-    <LoadScriptNext googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY} >
+    <LoadScriptNext googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY} preventGoogleFontsLoading={true} >
       <GoogleMap zoom={16} center={center} mapContainerClassName={className}>
         <MarkerF position={center}/>
       </GoogleMap>
