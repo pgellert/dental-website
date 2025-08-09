b8d13531b61461673e0b677580208f0c1246e4f3
Gellért Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2025-08-09 20:14:25 +0200

    improve image alt and links

--- src/markdown/hu/dental-services/cosmetic-dentistry.md
+++ src/markdown/hu/dental-services/cosmetic-dentistry.md
@@ -2,9 +2,9 @@
 
 A Perident fogászat kozmetikai fogászati ​​szolgáltatások széles választékát kínálja, többek között az implantátumokat, koronákat és hidakat, inlayeket és onlayeket.
 
-### [Implantátumok](/fogimplantatum-gyor)
+### [Fogimplantátum](/fogimplantatum-gyor)
 
-A fogászati ​​implantátum egy kis fém (titánium) csavar, amelyet műtéti úton az ínyvonal alatti állcsontba helyeznek, hogy a fog gyökereként szolgáljon. Számos különböző anyag, technika és fogászati ​​implantátum felépítés közül választhatnak a páciensek. Ez az weboldal bemutat néhányat a nálunk választható lehetőségek közül. A fogászati ​​implantátumokkal kapcsolatos további információkért kattintson [ide](/fogimplantatum-gyor).
+A fogászati ​​implantátum egy kis fém (titánium) csavar, amelyet műtéti úton az ínyvonal alatti állcsontba helyeznek, hogy a fog gyökereként szolgáljon. Számos különböző anyag, technika és fogászati ​​implantátum felépítés közül választhatnak a páciensek. Ez az weboldal bemutat néhányat a nálunk választható lehetőségek közül.
 
 ### [Koronák és hidak](/dental-services/cosmetic-dentistry/crowns-and-bridges-gyor)
 
--- src/pages/fogimplantatum-gyor.tsx
+++ src/pages/fogimplantatum-gyor.tsx
@@ -5,21 +5,24 @@ const data = {
   en: {
     title: "Dental Implants in Gyor | Perident Dentistry",
     meta_description: "Dental Implant Győr - Dr. Peresztegi Szabolcs 20+ years of experience, 3D CBCT planning, Guarantee, Free consultation, Nobel & Straumann implant.",
+    image_alt: "Dental implant placement in Győr at the Perident Dental Clinic",
   },
   hu: {
     title: "Fogimplantátum Győr | Perident Fogászat",
     meta_description: "Fogimplantátum Győr - Dr. Peresztegi Szabolcs 20+ év tapasztalat, 3D CBCT tervezés, Garancia, Ingyenes konzultáció, Nobel & Straumann implantátum.",
+    image_alt: "Fogimplantátum beültetés Győrben a Perident Fogászat rendelőjében",
   },
   de: {
     title: "Zahnimplantate in Györ | Zahnarztpraxis Perident",
     meta_description: "Zahnimplantat Győr – Dr. Peresztegi Szabolcs. Über 20 Jahre Erfahrung, 3D CBCT, Garantie, kostenlose Beratung, Nobel & Straumann-Implantat.",
+    image_alt: "Zahnimplantation in Győr in der Perident Zahnklinik",
   },
 }
 
 export default function Home({markdown, locale}) {
   const content = data[locale];
   return (
-    <ServicePage title={content.title} markdown={markdown} meta_description={content.meta_description} image={dentalImplants}/>
+    <ServicePage title={content.title} markdown={markdown} meta_description={content.meta_description} image={dentalImplants} image_alt={content.image_alt}/>
   )
 }
 
